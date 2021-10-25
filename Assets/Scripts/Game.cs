0f541a96ccbe246d15680141f27cec6248b5fcac
FawazAhmed-UTD <fxa180017@utdallas.edu>
2021-10-24 20:43:18 -0500

    Grid, blocks, environment, and basic textures added.
    Added a grid, basic tetris blocks, 3d environment, and textures to the game.

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public static int gridWidth = 10;
+    public static int gridHeight = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Tetromino.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tetromino : MonoBehaviour
+{
+    float fall = 0;
+    public float fallSpeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckUserInput();
+    }
+
+    void CheckUserInput()
+	{
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+            transform.position += new Vector3(1, 0, 0);
+		}
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+            transform.position += new Vector3(-1, 0, 0);
+		}
+        else if(Input.GetKeyDown(KeyCode.UpArrow))
+		{
+            transform.Rotate(0, 0, 90);
+		}
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fall >= fallSpeed)
+		{
+            transform.position += new Vector3(0, -1, 0);
+
+            fall = Time.time;
+		}
+	}
+}
